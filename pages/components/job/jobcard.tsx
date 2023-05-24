6ee1abe6bcb39e81bc5c7da2c1f1d709bc085b62
kelwingravhl <kelwin.tantono@gravhl.com>
2023-05-24 15:06:36 +0700

    update layouts and folder structure

---
+++ pages/components/job/jobcard.tsx
@@ -0,0 +1,92 @@
+import {
+  Badge,
+  Button,
+  Center,
+  Flex,
+  Heading,
+  Image,
+  Link,
+  Stack,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+
+export default function JobCard() {
+  return (
+    <Center py={6}>
+      <Stack
+        borderWidth="1px"
+        borderRadius="lg"
+        w={{ sm: "100%", md: "700px" }}
+        height={{ sm: "476px", md: "20rem" }}
+        direction={{ base: "column", md: "row" }}
+        bg={useColorModeValue("white", "gray.900")}
+        boxShadow={"2xl"}
+        padding={4}
+      >
+        <Flex flex={1} bg="blue.200">
+          <Image
+            objectFit="cover"
+            boxSize="100%"
+            src={
+              "https://remoteok.com/cdn-cgi/image/format=auto,fit=contain,width=300,height=300,quality=80/https://remoteOK.com/assets/img/jobs/968d4e4faa688a4304dac72703360f711677750913.png"
+            }
+          />
+        </Flex>
+        <Stack
+          flex={1}
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          p={1}
+          pt={2}
+        >
+          <Heading fontSize={"2xl"} fontFamily={"body"}>
+            Sticker Mule
+          </Heading>
+          <Text fontWeight={600} color={"gray.500"} size="sm" mb={4}>
+            Software Engineer
+          </Text>
+          <Stack align={"center"} justify={"center"} direction={"row"} mt={6}>
+            <Badge
+              px={2}
+              py={1}
+              bg={useColorModeValue("gray.50", "gray.800")}
+              fontWeight={"400"}
+            >
+              Software Engineer
+            </Badge>
+          </Stack>
+
+          <Stack
+            width={"100%"}
+            mt={"2rem"}
+            direction={"row"}
+            padding={2}
+            justifyContent={"space-between"}
+            alignItems={"center"}
+          >
+            <Button
+              flex={1}
+              fontSize={"sm"}
+              rounded={"full"}
+              bg={"blue.400"}
+              color={"white"}
+              boxShadow={
+                "0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)"
+              }
+              _hover={{
+                bg: "blue.500",
+              }}
+              _focus={{
+                bg: "blue.500",
+              }}
+            >
+              Apply
+            </Button>
+          </Stack>
+        </Stack>
+      </Stack>
+    </Center>
+  );
+}
